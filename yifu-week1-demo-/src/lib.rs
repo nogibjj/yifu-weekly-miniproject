e86677991926e277ce6af4aa053af6cb35e9bd5b
yifu <77519205+yifu-deng@users.noreply.github.com>
2023-02-25 19:36:25 -0500

    Delete yifu-week1-demo- directory

--- yifu-week1-demo-/src/lib.rs
+++
@@ -1,6 +0,0 @@
-// Create a dice function where six-sided dice are thrown, and each side has an equal probability of appearing
-pub fn squarenum(number: &str) -> String {
-    let num: i32 = number.parse().unwrap();
-    let result = num * num;
-    result.to_string()
-}
